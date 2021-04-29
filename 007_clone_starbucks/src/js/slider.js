d4ceba03ccb7f0632a9bd25c3d66c2cd7b1ea9b6
robertoalvesneto <robertoalvesnetoprodut@gmail.com>
2021-04-29 08:51:16 -0400

    clone starbucks

---
+++ 007_clone_starbucks/src/js/slider.js
@@ -0,0 +1,37 @@
+var slideIndex = 0;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function currentDiv(n) {
+  showDivs((slideIndex = n));
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("badge");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" badge-white", "");
+  }
+  x[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " badge-white";
+}
+
+carousel();
+
+function carousel() {
+  showDivs((slideIndex += 1));
+  setTimeout(carousel, 4000);
+}
